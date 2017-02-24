dd8e3bf8bb60b26e8d32ae5eec03e49f0f300a2a
root <root@localhost.localdomain>
2017-02-24 10:32:26 +0800

    123

--- machine.py
+++ machine.py
@@ -33,35 +33,25 @@ for index,i in enumerate(x):
 	plot.plot([i,i],[y[index],yr[index]],'o-')
 #plot.show()
 import numpy
-print numpy.mean((model.predict(x) - y) ** 2)
+#print numpy.mean((model.predict(x) - y) ** 2)
+print numpy.mean([1,2,3,4,5])
+
+ 
+
 # import numpy as np
-
 # import matplotlib.pyplot as plt
-
 # plt.figure(1) # 创建图表1
-
 # plt.figure(2) # 创建图表2
-
 # ax1 = plt.subplot(211) # 在图表2中创建子图1
-
 # ax2 = plt.subplot(212) # 在图表2中创建子图2
-
 # x = np.linspace(0, 3, 100)
-
 # for i in xrange(5):
-
 #     plt.figure(1)  #❶ # 选择图表1
-
 #     plt.plot(x, np.exp(i*x/3))
-
 #     plt.sca(ax1)   #❷ # 选择图表2的子图1
-
 #     plt.plot(x, np.sin(i*x))
-
 #     plt.sca(ax2)  # 选择图表2的子图2
-
 #     plt.plot(x, np.cos(i*x))
-
 # plt.show()
 
 
